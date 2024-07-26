ab36dd418e815eb3a5baf97c1dff0d821be969b2
jovan-stojkovic <code.jovanstojkovic@gmail.com>
2024-07-26 02:19:08 +0200

    swiper almost done

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import "../Styles/Products.scss";
 import { motion } from "framer-motion";
+import SwiperComponent from "../Components/Swiper";
 
 const Products = () => {
   const sectionMotion = {
@@ -29,64 +30,61 @@ const Products = () => {
         whileInView="visible"
         viewport={{ amount: 0.4, once: true }}
       >
-        <div className="text-part">
-          <h1>Garnišne za zavese</h1>
-          <h3>
-            Ukrasne garnišne za zavese - elegancija i funkcionalnost u vašem
-            enterijeru.
-          </h3>
-          <p>
-            Dodajte dašak sofisticiranosti i stila vašem prostoru uz naše
-            predivne ukrasne garnišne za zavese. Naša kolekcija garnišni nudi
-            savršenu kombinaciju estetike i funkcionalnosti, pružajući ne samo
-            podršku vašim zavesama već i ističući jedinstven karakter vašeg
-            enterijera.
-          </p>
-          <h5>Zašto izabrati naše garnišne?</h5>
-          <ul>
-            <li>
-              Kvalitetan materijal: Naše garnišne izrađene su od
-              visokokvalitetnih materijala koji osiguravaju dugotrajnu
-              izdržljivost i stabilnost.
-            </li>
-            <li>
-              Elegantan dizajn: S širokim spektrom dizajna i završnih obrada, od
-              klasičnih do modernih stilova, pronaći ćete garnišne koje se
-              savršeno uklapaju u svaki prostor.
-            </li>
-            <li>
-              Jednostavna instalacija: Dizajnirane za jednostavnu i brzu
-              montažu, naše garnišne omogućavaju lako postavljanje i
-              prilagođavanje.
-            </li>
-            <li>
-              Funkcionalnost i stvaranje atmosfere: Osim što podržavaju vaše
-              zavese, naš izbor garnišni doprinosi stvaranju željene atmosfere,
-              bilo da se radi o sofisticiranom, minimalističkom ili rustikalnom
-              izgledu.
-            </li>
-          </ul>
-          <h5>Pretražite našu ponudu</h5>
-          <p>
-            Istražite našu kolekciju i pronađite savršene ukrasne garnišne za
-            zavese koje će unaprediti izgled vašeg prostora. Uz kvalitet i
-            dizajn koji su u centru naše ponude, garantujemo da ćete pronaći
-            rešenje koje odgovara vašim potrebama i estetskim željama.
-          </p>
-          <h5>Kontaktirajte nas</h5>
-          <p>
-            Imate pitanja ili vam treba pomoć u odabiru? Naš tim stručnjaka je
-            ovde da vam pomogne.
-          </p>
+        <h1>Garnišne za zavese</h1>
+        <h3>
+          Ukrasne garnišne za zavese - elegancija i funkcionalnost u vašem
+          enterijeru.
+        </h3>
+        <p>
+          Dodajte dašak sofisticiranosti i stila vašem prostoru uz naše predivne
+          ukrasne garnišne za zavese. Naša kolekcija garnišni nudi savršenu
+          kombinaciju estetike i funkcionalnosti, pružajući ne samo podršku
+          vašim zavesama već i ističući jedinstven karakter vašeg enterijera.
+        </p>
+        <h5>Zašto izabrati naše garnišne?</h5>
+        <ul>
+          <li>
+            Kvalitetan materijal: Naše garnišne izrađene su od visokokvalitetnih
+            materijala koji osiguravaju dugotrajnu izdržljivost i stabilnost.
+          </li>
+          <li>
+            Elegantan dizajn: S širokim spektrom dizajna i završnih obrada, od
+            klasičnih do modernih stilova, pronaći ćete garnišne koje se
+            savršeno uklapaju u svaki prostor.
+          </li>
+          <li>
+            Jednostavna instalacija: Dizajnirane za jednostavnu i brzu montažu,
+            naše garnišne omogućavaju lako postavljanje i prilagođavanje.
+          </li>
+          <li>
+            Funkcionalnost i stvaranje atmosfere: Osim što podržavaju vaše
+            zavese, naš izbor garnišni doprinosi stvaranju željene atmosfere,
+            bilo da se radi o sofisticiranom, minimalističkom ili rustikalnom
+            izgledu.
+          </li>
+        </ul>
+        <h5>Pretražite našu ponudu</h5>
+        <p>
+          Istražite našu kolekciju i pronađite savršene ukrasne garnišne za
+          zavese koje će unaprediti izgled vašeg prostora. Uz kvalitet i dizajn
+          koji su u centru naše ponude, garantujemo da ćete pronaći rešenje koje
+          odgovara vašim potrebama i estetskim željama.
+        </p>
+        <h5>Kontaktirajte nas</h5>
+        <p>
+          Imate pitanja ili vam treba pomoć u odabiru? Naš tim stručnjaka je
+          ovde da vam pomogne.
+        </p>
 
-          {/* <Link
+        {/* <Link
             to="/proizvodi/kategorija-jedan"
             onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
           >
             Pogledaj kompletnu ponudu
           </Link> */}
+        <div className="swiper-part">
+          <SwiperComponent />
         </div>
-        <div className="img-part"></div>
       </motion.section>
       <motion.section
         className="vanilla-section"
